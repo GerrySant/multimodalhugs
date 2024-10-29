[FIX] Fixed the padding management for loss computation, which caused Transformer based models to not properly train because the decoder was able to use tokens that it should not when using teacher forcing. As a result, the model losses decreased correctly, but in generation, the encoder output information was not used for predictions.

multimodalhugs/data/multimodal_datacollator.py
@@ -1,17 +1,113 @@
 import torch
 
 from dataclasses import dataclass
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Union, Optional
+from transformers.utils import PaddingStrategy
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 
 
 @dataclass
 class DataCollatorMultimodalSeq2Seq:
     processor: Any
+    tokenizer: PreTrainedTokenizerBase
+    model: Optional[Any] = None
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    label_pad_token_id: int = -100
+    return_tensors: str = "pt"
+
+    def __init__(
+        self,
+        processor: Any,
+        tokenizer: Optional[PreTrainedTokenizerBase] = None,
+        model: Optional[Any] = None,
+        padding: Union[bool, str, PaddingStrategy] = True,
+        max_length: Optional[int] = None,
+        pad_to_multiple_of: Optional[int] = None,
+        label_pad_token_id: int = -100,
+        return_tensors: str = "pt"
+    ):
+        self.processor = processor
+        self.tokenizer = tokenizer if tokenizer is not None else processor.tokenizer
+        self.model = model
+        self.padding = padding
+        self.max_length = max_length
+        self.pad_to_multiple_of = pad_to_multiple_of
+        self.label_pad_token_id = label_pad_token_id
+        self.return_tensors = return_tensors
+
+    def _obtain_text_related_inputs_outputs(self, samples, return_tensors=None):
+        batch = {}
+        if return_tensors is None:
+            return_tensors = self.return_tensors
+
+        no_padding = self.padding is False or self.padding == PaddingStrategy.DO_NOT_PAD
+
+        labels = [
+            [self.tokenizer.convert_tokens_to_ids(f"__{sample['tgt_lang']}__")]
+            + self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(sample['tgt_sentence']))
+            + [self.tokenizer.eos_token_id]
+            for sample in samples]
+
+        if labels is not None:
+            if no_padding:
+                batch["labels"] = list(labels)
+            else:
+                max_padding = self.padding == PaddingStrategy.MAX_LENGTH and self.max_length is not None
+                max_label_length = max(len(l) for l in labels) if not max_padding else self.max_length
+                if self.pad_to_multiple_of is not None:
+                    max_label_length = (
+                        (max_label_length + self.pad_to_multiple_of - 1)
+                        // self.pad_to_multiple_of
+                        * self.pad_to_multiple_of
+                    )
+
+                padding_side = self.tokenizer.padding_side
+                batch["labels"] = [
+                    label + [self.label_pad_token_id] * (max_label_length - len(label))
+                    if padding_side == "right"
+                    else [self.label_pad_token_id] * (max_label_length - len(label)) + label
+                    for label in labels
+                ]
+
+        # reintroduce side effects via tokenizer that return respective datatypes for the `return_tensors` argument
+        if batch.get("labels", None) is not None:
+            if return_tensors == "pt":
+                import torch
+
+                batch["labels"] = torch.tensor(batch["labels"], dtype=torch.int64)
+            elif return_tensors == "tf":
+                import tensorflow as tf
+
+                batch["labels"] = tf.constant(batch["labels"], dtype=tf.int64)
+            else:
+                import numpy as np
+                batch["labels"] = np.array(batch["labels"], dtype=np.int64)
+        else:
+            batch["labels"] = None
+
+        # prepare decoder_input_ids
+        if (
+            labels is not None
+            and self.model is not None
+            and hasattr(self.model, "prepare_decoder_input_ids_from_labels")
+        ):
+            decoder_input_ids = self.model.prepare_decoder_input_ids_from_labels(labels=batch["labels"])
+            batch["decoder_input_ids"] = decoder_input_ids
+
+        return batch
 
     def __call__(
         self, samples: List[Dict[str, Union[List[int], torch.Tensor]]]
     ) -> Dict[str, torch.Tensor]:
 
-        batch = self.processor(samples, return_tensors="pt")
+        batch_dict = self._obtain_text_related_inputs_outputs(samples)
+
+        batch = self.processor(
+            batch=samples,
+            batch_dict=batch_dict,
+            return_tensors="pt"
+        )
 
         return batch

multimodalhugs/processors/multimodal_secuence2text_processor.py
@@ -79,44 +79,13 @@ class MultimodalSecuence2TextTranslationProcessor(ProcessorMixin):  # FeatureExt
     def _obtain_others(self, batch, **kwargs):
         return self._obtain_src_langtoks(batch, **kwargs)
         
-    def _obtain_text_inputs_and_labels(self, batch, **kwargs):
-        tgt_langtoks = torch.stack(
-            [torch.LongTensor([self.get_langtok(f"__{sample['tgt_lang']}__")]) for sample in batch]
-        )
-
-        tokenization = self.tokenizer(
-            text=[sample["tgt_sentence"] for sample in batch],
-            return_tensors='pt', 
-            padding=True, 
-            truncation=True,
-            return_attention_mask=True,
-            add_special_tokens=True,
-        )
-        
-        tgt_tensor = tokenization['input_ids']                              # ['<token_a>', '<token_b>', '<token_c>', '</s>']
-        tgt_tensor = torch.cat((tgt_langtoks, tgt_tensor[:, 1:]), dim=1)    # ['<tgt_lang>', '<token_a>', '<token_b>', '<token_c>', '</s>']
-
-        decoder_attention_mask = tokenization['attention_mask']
-        decoder_attention_mask = torch.cat((torch.full((decoder_attention_mask.size(0), 1), 1), decoder_attention_mask), dim=1)
-        decoder_attention_mask = decoder_attention_mask[..., :-1].contiguous()
-        
-        # Prepare final output for model consumption
-        decoder_input_ids = torch.full((tgt_tensor.size(0), 1), self.tokenizer.convert_tokens_to_ids(self.tokenizer.eos_token))
-        decoder_input_ids = torch.cat((decoder_input_ids, tgt_tensor), dim=1)   # ['</s>', '<tgt_lang>', '<token_a>', '<token_b>', '<token_c>', '</s>']
-        decoder_input_ids = decoder_input_ids[..., :-1].contiguous()            # ['</s>', '<tgt_lang>', '<token_a>', '<token_b>', '<token_c>']
-        return {
-            "decoder_input_ids": decoder_input_ids,                # torch.Size([batch_size, n_tokens])
-            "labels": tgt_tensor,                                  # torch.Size([batch_size, n_tgt_tokens])
-            "decoder_attention_mask": decoder_attention_mask,      # torch.Size([batch_size, n_tokens]) 0 indicates padding elements   
-        }, kwargs
-        
     def __call__(
         self,
         batch: List[Dict[str, Any]],
+        batch_dict: Optional[Dict[str, Any]] = {},
         **kwargs,
     ) -> BatchFeature:
-        
-        batch_dict = {}
+
         for obtain_method in self.get_obtainables():
             obgained_dict, kwargs = obtain_method(batch, **kwargs)
             batch_dict.update(obgained_dict)