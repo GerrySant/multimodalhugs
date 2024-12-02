7f0c0ab3a9b2b68ec5b253bd1708678a1284372c
Sant Muniesa Gerard (gsantm) <gerard.sant.muniesa@gmail.com>
2024-12-02 12:44:03 +0100

    [UPD] The file organization within multimodalhugs/data has been improved.

--- multimodalhugs/data/__init__.py
+++ multimodalhugs/data/__init__.py
@@ -1,15 +1,13 @@
 from .utils import *
-from .data_configs import (
+from .dataset_configs.data_configs import (
     PreprocessArguments, 
     MultimodalMTDataConfig, 
     SignLanguageMTDataConfig, 
     BilingualMTDataConfig,
     BilingualImage2textMTDataConfig,
 )
-from .signwriting import SignWritingDataset
-from .how2sign import How2SignDataset
-from .bilingual_text2text import BilingualText2TextDataset
-from .bilingual_image2text import BilingualImage2TextDataset
-from .multimodal_datacollator import DataCollatorMultimodalSeq2Seq
-
-
+from .datasets.signwriting import SignWritingDataset
+from .datasets.how2sign import How2SignDataset
+from .datasets.bilingual_text2text import BilingualText2TextDataset
+from .datasets.bilingual_image2text import BilingualImage2TextDataset
+from .datacollators.multimodal_datacollator import DataCollatorMultimodalSeq2Seq
